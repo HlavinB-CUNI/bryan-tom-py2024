ea7fb8e8d77a5593cd3216acf0b435a0b26f9a87
HlavinB-CUNI <150315423+Izeawk@users.noreply.github.com>
2024-12-01 20:41:40 +0100

    Finish index info grabbing and add in URL parser

--- Python2024_Final_Project/Python2024_Final_Project/olymp_funcs.py
+++ Python2024_Final_Project/Python2024_Final_Project/olymp_funcs.py
@@ -17,6 +17,10 @@ def is_valid_olympic_year(year, games_json):
         return False    
 
 def scrape_olympics_websites(start_games, end_games, szn, df_olymp):
+    # splitting the URL to prevent IDE issues
+    url_1 = "https://"
+    url_2 = "olympics.com/en/olympic-games/"
+
     # making a dataframe from the dictionary (2 rows, 38 columns for every olympics)
     df_olymp.index = df_olymp.index.astype(int)
     df_filter1 = df_olymp.loc[pd.to_numeric(start_games):pd.to_numeric(end_games)]
@@ -26,38 +30,50 @@ def scrape_olympics_websites(start_games, end_games, szn, df_olymp):
     for city, season in enumerate(df_filter2['seasons']):
         # if the season is summer
         if ((('summer' in season)) & (szn == 'summer')):
-            print(df_filter2['city'].iloc[city][0]) 
-            print(validators.url(f'https://olympics.com/en/olympic-games/paris-2024/medals?displayAsWebViewlight=true&displayAsWebView=true'))
+            print(df_filter2.index[city]) # for year
+            print(df_filter2['city'].iloc[city][0]) # for city
+            url_specific = f"{url_1}{url_2}{df_filter2['city'].iloc[city][0]}-{df_filter2.index[city]}/medals?displayAsWebViewlight=true&displayAsWebView=true"
             
+            req = Request(
+            url= url_specific, 
+            headers={'User-Agent': 'Mozilla/5.0'})
+            page = urlopen(req).read()
+            soup = BeautifulSoup(page, 'html.parser')
+            soup_medalfilt = soup.find_all(title= "Gold")
+            soup_filt = soup.find_all(class_ = "OcsText-styles__StyledText-sc-bf256156-0 cjPVFu text--sm-body")
+            print(soup_medalfilt)
+            time.sleep(random.uniform(0.05, 0.5))
+
         # if the season is winter
         elif ((('winter' in season)) & (szn == 'winter')):
             if (len(df_filter2['city'].iloc[city]) == 1):
-                print(df_filter2['city'].iloc[city][0])
-                print(validators.url(f'https://olympics.com/en/olympic-games/paris-2024/medals?displayAsWebViewlight=true&displayAsWebView=true'))
-            else:
-                print(df_filter2['city'].iloc[city][1]) # winter olympic location (if 2 locations in a year) is always in the 2nd index location
-                print(validators.url(f'https://olympics.com/en/olympic-games/paris-2024/medals?displayAsWebViewlight=true&displayAsWebView=true'))
+                print(df_filter2.index[city]) # for year
+                print(df_filter2['city'].iloc[city][0]) # for city
+                url_specific = f"{url_1}{url_2}{df_filter2['city'].iloc[city][0]}-{df_filter2.index[city]}/medals?displayAsWebViewlight=true&displayAsWebView=true"
                 
-            
-    # cycle through the dataframe, scraping each website
-    #for 
-    #req = Request(url=f'https://olympics.com/en/olympic-games/paris-2024/medals?displayAsWebViewlight=true&displayAsWebView=true', 
-#    headers={'User-Agent': 'Mozilla/5.0'}
-#))
-
-
+                req = Request(
+                url= url_specific, 
+                headers={'User-Agent': 'Mozilla/5.0'})
+                page = urlopen(req).read()
+                soup = BeautifulSoup(page, 'html.parser')
+                soup_medalfilt = soup.find_all(title= "Gold")
+                soup_filt = soup.find_all(class_ = "OcsText-styles__StyledText-sc-bf256156-0 cjPVFu text--sm-body")
+                print(soup_medalfilt)
+                time.sleep(random.uniform(0.05, 0.5))
+            else:
+                print(df_filter2.index[city]) # for year
+                print(df_filter2['city'].iloc[city][1]) # winter olympic location (if 2 locations in a year) is always in the 2nd index location
+                url_specific = f"{url_1}{url_2}{df_filter2['city'].iloc[city][0]}-{df_filter2.index[city]}/medals?displayAsWebViewlight=true&displayAsWebView=true"
+                
+                req = Request(
+                url= url_specific, 
+                headers={'User-Agent': 'Mozilla/5.0'})
+                page = urlopen(req).read()
+                soup = BeautifulSoup(page, 'html.parser')
+                soup_medalfilt = soup.find_all(title= "Gold")
+                soup_filt = soup.find_all(class_ = "OcsText-styles__StyledText-sc-bf256156-0 cjPVFu text--sm-body")
+                print(soup_medalfilt)
+                time.sleep(random.uniform(0.05, 0.5))
+                
 
     
-#req_2016 = Request(
-#    url='https://olympics.com/en/olympic-games/paris-2024/medals?displayAsWebViewlight=true&displayAsWebView=true', 
-#    headers={'User-Agent': 'Mozilla/5.0'}
-#)
-#page = urlopen(req_2016).read()
-#soup = BeautifulSoup(page, 'html.parser')
-#soup_medalfilt = soup.find_all(title= "Gold")
-#soup_filt = soup.find_all(class_ = "OcsText-styles__StyledText-sc-bf256156-0 cjPVFu text--sm-body")
-#print(soup_medalfilt)
-#time.sleep(random.uniform(0.05, 0.5))
-    
-
-        
