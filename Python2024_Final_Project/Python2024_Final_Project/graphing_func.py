a42363befcfce64fc5cd063784d7a503b2eea826
TommasoFazzi <48465320@fsv.cuni.cz>
2025-02-02 18:26:44 +0100

    starting to do graphing

---
+++ Python2024_Final_Project/Python2024_Final_Project/graphing_func.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_medal_histogram(df_total):
+    """
+    Generate a histogram of total medals won by countries.
+    :param df_total: DataFrame containing medal counts per country
+    """
+
+    if df_total is None or df_total.empty:
+        print("No data available to plot.")
+        return
+    
+    # Summing up all medals (gold, silver, bronze) for each country
+    df_total['results'] = df_total.filter(like='2002_total').sum(axis=1)
+    df_total['2002_total'] = pd.to_numeric(df_total['2002_total'])
+    
+    # Sorting by total medals
+    df_total_sorted = df_total.sort_values(by='2002_total', ascending=False)
+
+    # Selecting top 10 countries for better visualization
+    top_countries = df_total_sorted.head(10)
+    print(df_total)
+
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.bar(top_countries.index, top_countries['2002_total'], color='gold')
+    plt.xlabel('Country')
+    plt.ylabel('results')
+    plt.title('Top 10 Countries by Total Medals')
+    plt.xticks(rotation=45)
+    plt.show()
